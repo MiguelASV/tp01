2e142031aca71f210765245804d4d059fedaa1c8
Miguel Santana <miguelsantana@hotmail.ca>
2020-02-17 17:59:54 -0500

    Remodification des fichiers de travail

---
+++ mjs/config_slider.mjs
@@ -0,0 +1,35 @@
+export const config_slider = {
+
+    largeur : '500px',
+    hauteur : '500px',
+
+    list_slide : [
+        {
+            titre : 'Le Titre',
+            sousTitre : 'Le sous titre',
+            img : './img/html5.png'
+        },
+        {
+            titre : 'Le Titre',
+            sousTitre : 'Le sous titre',
+            img : './img/css3.png'
+        },
+        {
+            titre : 'Le Titre',
+            sousTitre : 'Le sous titre',
+            img : './img/js.png'
+        },
+        {
+            titre : 'Le Titre',
+            sousTitre : 'Le sous titre',
+            img : './img/php.png'
+        },
+        {
+            titre : 'Le Titre',
+            sousTitre : 'Le sous titre',
+            img : './img/cSharp.png'
+        }
+
+
+    ]
+}
